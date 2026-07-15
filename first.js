572db2af5e2d5f32a25ec818433d741ace58c464
omprakash panda <ommprakashp78@gmail.com>
2026-07-15 08:25:37 +0530

    My first commit

---
+++ first.js
@@ -0,0 +1,15 @@
+let score = prompt("Enter your score: ");
+
+if(score >= 90 && score <= 100) {
+    console.log("A grade");
+}else if(score >= 70 && score <= 89) {
+    console.log("B grade");
+}else if(score >= 50 && score <= 69){
+    console.log("C grade");
+}else if(score >= 35 && score <= 49){
+    console.log("D grade");
+}else if(score >= 0 && score <= 34){
+    console.log("fail");
+}else{
+    console.log("not found");
+}
