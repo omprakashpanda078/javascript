0fe82e4eb38dbae49a9c1c7a786c8ebcc9e0cf74
omprakash panda <ommprakashp78@gmail.com>
2026-07-17 19:45:13 +0530

    get your username

--- first.js
+++ first.js
@@ -1,15 +1,7 @@
-let gameNum = 60;
- let userNum = prompt("guess the number :");
+let fulName = prompt("Enter your name without space");
 
- while ( userNum != gameNum) {
-    userNum = prompt("you guess the wrong number try again :");
-    userNum = prompt("again wrong number try again :");
-    userNum = prompt("this is not expected from you :");
-    userNum = prompt("you are not eligiable to play this game :");
-    userNum = prompt("you are noob in ff not here try again:");
-    userNum = prompt("you are really noob the num is 59 :");
-    userNum = prompt("do you think i give you the right num ha ha ha :");
-    userNum = prompt("let give you a hint you are very close to the number :");
- }
 
- console.log("great you guess the right number");
+let userName = "@"+fulName+fulName.length;
+
+
+console.log(userName);
