13adb908d9ac71b42ae0e0a6ef9b1e911b18df8d
omprakash panda <ommprakashp78@gmail.com>
2026-07-17 22:41:25 +0530

    problem

--- first.js
+++ first.js
@@ -1,10 +1,11 @@
-let marks = [85,97,44,37,58,60];
+let price = [250,645,300,900,50];
 
-let sum = 0;
-
-for(let i = 0; i<marks.length; i++){
-    console.log(marks[i]); 
-    sum = sum + marks[i];
+// formula discount = price/10 finalPrice = discount - offer
+for(let i=0; i<price.length; i++){  
+    let currPrice = price[i];
+    let discount = currPrice/10;
+    let finalPrice = currPrice - discount;
+    // updating finalPrice at each respective index
+    price[i]=finalPrice;
 }
-let avg = sum / marks.length;
-console.log("the avg of the total no. is ", avg)
+console.log(price);
