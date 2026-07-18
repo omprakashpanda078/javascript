4904da2ff89cedff07be432ed61395f1aaf361fb
omprakash panda <ommprakashp78@gmail.com>
2026-07-18 08:18:50 +0530

    Largest Number In a Array Done!

--- first.js
+++ first.js
@@ -9,7 +9,15 @@ let companyName = ["bloomberg","microsoft","uber","google","IBM","netflix"];
 
 //add amazone at the end
 
-companyName.pop()
+// companyName.push("Amazone")
 
 
-console.log(companyName)
+// console.log(companyName)
+
+// Largest Element in an array
+let arr = [1, 2, 3, 6, 24, 8, 7, 98];
+let largest = arr[0];
+for(let i = 1; i < arr.length; i++) {
+    if(arr[i] > largest) largest=arr[i];
+}
+console.log(largest);
