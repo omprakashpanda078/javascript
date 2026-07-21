f36cc6010c5bca33fd86e90feb4d67126b6ab98e
omprakash panda <ommprakashp78@gmail.com>
2026-07-21 22:35:04 +0530

    arraysum

---
+++ arraysum.js
@@ -0,0 +1,12 @@
+function arraysum(arr){
+
+    let sum = 0;
+
+    for(let i = 0; i < arr.length; i++){
+
+    sum = sum + arr[i];
+
+    }
+    console.log("The sum of the array is : ",sum);
+}
+  arraysum([25,41,14,36,98,78,]);
