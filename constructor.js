719f022023436ed570a483ca4dcc88b60b384057
omprakash panda <ommprakashp78@gmail.com>
2026-08-01 20:27:15 +0530

    constructor

---
+++ constructor.js
@@ -0,0 +1,9 @@
+ class Student{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;    }
+ }
+ let student1 = new Student("omprakash panda",20);
+
+ console.log(student1.name);
+ console.log(student1.age);
